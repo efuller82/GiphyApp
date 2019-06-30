01889fbd16bfe6bf8d36c914216c0e9cc71f7eb7
Eric Fuller <efuller82@gmail.com>
2019-06-29 23:40:16 -0500

    still trying to make contact to API

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+var giph;
+//something is off here
+var queryURL = "https://api.giphy.com/v1/gifs/funny?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=&limit=10&offset=0&rating=G&lang=en";
+
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+})
