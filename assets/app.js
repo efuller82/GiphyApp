cdac11eb5907cbde6598d25bad2c03d01434b1e7
Eric Fuller <efuller82@gmail.com>
2019-06-29 23:43:06 -0500

    made contact :)

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 var giph;
 //something is off here
-var queryURL = "https://api.giphy.com/v1/gifs/funny?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=&limit=10&offset=0&rating=G&lang=en";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=cats&limit=10&offset=0&rating=G&lang=en";
 
 $.ajax ({
     url: queryURL,
