3e95141527be19e6f756e9fe87aeb62a86299edd
Eric Fuller <efuller82@gmail.com>
2019-07-01 11:28:53 -0500

    added click event

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,29 @@
-var giph;
-//something is off here
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=cats&limit=10&offset=0&rating=G&lang=en";
+var APIkey = "BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"
 
-$.ajax ({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
+
+
+//array of my likes
+likesArray = ['South Park', 'funny faces', 'Trump', 'lazy', 'Forest Gump'];
+
+//calls the API
+
+
+//loop that creates buttons from the likesArray 
+for (var i = 0; i < likesArray.length; i++){
+    var buttons = $('<button>' + likesArray[i] + '</button>')
+    buttons.appendTo('.buttons');
+}
+
+//on click function 
+$('#giphy-search').on('click', function(event){
+    event.preventDefault();
+    var giphyInput = $('#giphy-search').val();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIkey + "&q=" + giphyInput + "&limit=10&offset=0&rating=G&lang=en";
+    
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
 })
